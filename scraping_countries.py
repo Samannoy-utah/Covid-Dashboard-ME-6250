f891f3eadb7a5f90919f0464b3ea1912436c35f1
Samannoy Ghosh <82288360+Samannoy-utah@users.noreply.github.com>
2022-11-29 22:20:24 -0700

    countryList updated to store all names

--- scraping_countries.py
+++ scraping_countries.py
@@ -12,9 +12,10 @@ from bs4 import BeautifulSoup
 URL = "https://www.worldometers.info/coronavirus/"
 page = requests.get(URL)
 
+countryList=[] # List to store the country names
 soup = BeautifulSoup(page.content, "html.parser")
 
 results = soup.find(id="main_table_countries_today")
 countries = results.find_all("a", class_="mt_a")
 for country in countries:
-    print(country.text)
+    countryList.append(country.text)
